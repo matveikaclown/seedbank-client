a0c30596ea18622e00d94628d1cce2a0bace9694
Matthew <privalka2003@mail.ru>
2025-02-05 21:14:30 +0400

    modified api for authentication

--- src/conf/api.js
+++ src/conf/api.js
@@ -2,33 +2,37 @@ import Cookies from "js-cookie";
 
 import axios from "axios";
 
+const BASE_URL = "http://localhost:8080/api";
+
 // Создаем экземпляр Axios
 const api = axios.create({
-    baseURL: "http://localhost:8080/api", // Замените на URL вашего API
-    withCredentials: true // Чтобы автоматически отправлялись куки
+    baseURL: BASE_URL,
+    withCredentials: true
 });
 
 // Добавляем интерцептор ответов
 api.interceptors.response.use(
-    (response) => response, // Успешный ответ возвращается как есть
+    (response) => response,
     async (error) => {
+        if (window.location.pathname === "/auth") {
+            return Promise.reject(error); 
+        }
+
         if (error.response && error.response.status === 403) {
             try {
-                // Отправляем запрос на обновление токенов
                 await api.post("/auth/refresh");
 
                 // Повторяем исходный запрос
                 return api.request(error.config);
             } catch (refreshError) {
-                // Если обновить токены не удалось, перенаправляем на страницу логина
                 Cookies.remove('UserData');
                 window.location.href = "/auth";
                 return Promise.reject(refreshError);
             }
         }
 
-        return Promise.reject(error); // Обработка других ошибок
+        return Promise.reject(error);
     }
 );
 
-export default api; // Экспортируем настроенный клиент
+export default api;
